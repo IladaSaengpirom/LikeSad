831625f48882e677c2b0ad70a934d842c9fa4ef3
SuttichaSonbalee <Suttichason123@gmail.com>
2023-01-27 19:51:43 +0700

    likesad1

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+
+</script>
+
+<template>
+
+
+<div class="row   row-cols-md-3 ">
+  <div class="col">
+    <div class="card">
+      <img src="https://i.pinimg.com/564x/dd/f6/5d/ddf65de84d28618695eb60a38f48238d.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"> </p>
+        <div>
+        <LikeSad></LikeSad>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://i.pinimg.com/564x/dd/f6/5d/ddf65de84d28618695eb60a38f48238d.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+        <div>
+        <LikeSad></LikeSad>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://i.pinimg.com/564x/dd/f6/5d/ddf65de84d28618695eb60a38f48238d.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+        <div>
+        <LikeSad></LikeSad>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://i.pinimg.com/564x/dd/f6/5d/ddf65de84d28618695eb60a38f48238d.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+        <div>
+        <LikeSad></LikeSad>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://i.pinimg.com/564x/dd/f6/5d/ddf65de84d28618695eb60a38f48238d.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+        <div>
+        <LikeSad></LikeSad>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://i.pinimg.com/564x/dd/f6/5d/ddf65de84d28618695eb60a38f48238d.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text"></p>
+        <div>
+        <LikeSad></LikeSad>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+</template>
